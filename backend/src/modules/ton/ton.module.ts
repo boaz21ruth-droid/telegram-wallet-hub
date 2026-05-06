b670e9aaa25ab3c6574bd93fd4c4ab7cfb9a54b0
cc <boaz21ruth@gmail.com>
2026-05-06 08:37:44 +0700

    feat(ton): update TonModule to include HotWalletService and WithdrawalBroadcasterService

--- backend/src/modules/ton/ton.module.ts
+++ backend/src/modules/ton/ton.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
 import { DepositsModule } from "../deposits/deposits.module";
+import { WithdrawalsModule } from "../withdrawals/withdrawals.module";
 import { DepositScannerService } from "./deposit-scanner.service";
+import { HotWalletService } from "./hot-wallet.service";
 import { TonService } from "./ton.service";
+import { WithdrawalBroadcasterService } from "./withdrawal-broadcaster.service";
 
 @Module({
-  imports: [DepositsModule],
-  providers: [TonService, DepositScannerService],
-  exports: [TonService],
+  imports: [DepositsModule, WithdrawalsModule],
+  providers: [TonService, DepositScannerService, HotWalletService, WithdrawalBroadcasterService],
+  exports: [TonService, HotWalletService],
 })
 export class TonModule {}
